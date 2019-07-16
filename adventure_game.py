6a9bf71ca77749d2527488eaa44fa121c1d523a9
Jagandeep Singh <jagansingh93@gmail.com>
2019-07-15 23:52:58 -0400

    pycodestyle errors fixed and play_again function added

--- adventure_game.py
+++ adventure_game.py
@@ -17,26 +17,33 @@ sword = False
 def house(name, weap):
     print("You approach the door of the house.")
     time.sleep(1.5)
-    print("You are about to knock when the door opens and out steps a " + name + ".")
+    print("You are about to knock when the door opens and"
+          " out steps a " + name + ".")
     time.sleep(1.5)
     print("Eep! This is the " + name + "\'s house!")
     time.sleep(1.5)
     print("The " + name + " attacks you!")
     time.sleep(1.5)
-    print("You feel a bit under-prepared for this, what with only having a tiny dagger.")
-    time.sleep(1.5)
+    if not weap:
+        print("You feel a bit under-prepared for this, "
+              "what with only having a tiny dagger.")
+        time.sleep(1.5)
     print("Would you like to (1) fight or (2) run away?")
     res = input("Please enter 1 or 2\n")
     while True:
         if res == "1":
             if weap:
-                print("As the " + name + " moves to attack, you unsheath your new sword.")
+                print("As the " + name + " moves to attack, "
+                                         "you unsheath your new sword.")
                 time.sleep(1.5)
-                print("The Sword of Ogoroth shines brightly in your hand as you brace yourself for the attack.")
+                print("The Sword of Ogoroth shines brightly in your hand"
+                      " as you brace yourself for the attack.")
                 time.sleep(1.5)
-                print("But the " + name + " takes one look at your shiny new toy and runs away!")
+                print("But the " + name + " takes one look at "
+                                          "your shiny new toy and runs away!")
                 time.sleep(1.5)
-                print("You have rid the town of the " + name + ". You are victorious!")
+                print("You have rid the town of the " + name +
+                      ". You are victorious!")
                 time.sleep(1.5)
             else:
                 print("You do your best...")
@@ -74,7 +81,8 @@ def cave(weapo):
         time.sleep(1.5)
         print("What would you like to do?")
     else:
-        print("You've been here before, and gotten all the good stuff. It's just an empty cave now.")
+        print("You've been here before, and gotten all the good stuff."
+              " It's just an empty cave now.")
         time.sleep(1.5)
         print("You walk back out to the field.")
         time.sleep(1.5)
@@ -97,7 +105,8 @@ def cave(weapo):
 
 
 def field():
-    print("You run back into the field. Luckily, you don't seem to have been followed.")
+    print("You run back into the field. Luckily, "
+          "you don't seem to have been followed.")
     time.sleep(1.5)
     print("")
     print("Enter 1 to knock on the door of the house.")
@@ -117,15 +126,34 @@ def field():
             num = input("Please enter 1 or 2\n")
 
 
+def play_again():
+    response = input("Would you like to play again? (y/n)\n")
+    while True:
+        if response == "y":
+            print("Excellent! Restarting the game\n")
+            time.sleep(2)
+            return True
+        elif response == "n":
+            print("Thanks for playing! See you next time.")
+            time.sleep(2)
+            return False
+        else:
+            response = input("Would you like to play again? (y/n)\n")
+    return False
+
+
 while firstTime or restart:
     enemy = random.choice(enemies)
     sword = False
     firstTime = False
     restart = False
     weapon = False
-    print("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
+    print("You find yourself standing in an open field,"
+          " filled with grass and yellow wildflowers.")
     time.sleep(1.5)
-    print("Rumor has it that a " + enemy + " is somewhere around here, and has been terrifying the nearby village.")
+    print("Rumor has it that a " + enemy +
+          " is somewhere around here, and"
+          " has been terrifying the nearby village.")
     time.sleep(1.5)
     print("In front of you is a house.")
     time.sleep(1.5)
@@ -153,17 +181,4 @@ while firstTime or restart:
         else:
             num = input("Please enter 1 or 2\n")
 
-# response is used to ask user to restart or end the game.
-    response = input("Would you like to play again? (y/n)\n")
-    while True:
-        if response == "y":
-            restart = True
-            print("Excellent! Restarting the game")
-            time.sleep(2)
-            break
-        elif response == "n":
-            print("Thanks for playing! See you next time.")
-            time.sleep(2)
-            break
-        else:
-            response = input("Would you like to play again? (y/n)\n")
+    restart = play_again()
